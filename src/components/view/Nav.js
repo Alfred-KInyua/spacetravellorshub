2cb908d18ba3bf186c8332027ffe8530a700cb0f
yayner <yayner2002@gmail.com>
2022-08-22 23:44:16 +0300

    Add nav to the header

---
+++ src/components/view/Nav.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import navStyle from './Nav.module.css';
+import planet from './planet.png';
+
+const Nav = () => (
+  <nav className={navStyle.nav}>
+    <div className={navStyle.navLeft}>
+      <img src={planet} alt="logo" className={navStyle.logo} />
+      <navLink to="/">
+        <h1>Space Travellers Hub</h1>
+      </navLink>
+    </div>
+    <div className={navStyle.navRight}>
+      <ul className={navStyle.navLinks}>
+        <li>
+          <NavLink to="/" className={navStyle.navLink}>
+            Rockets
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="missions" className={navStyle.navLink}>
+            Missions
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="myprofile" className={navStyle.navLink} id={navStyle.profile}>
+            Profile
+          </NavLink>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+);
+
+export default Nav;
