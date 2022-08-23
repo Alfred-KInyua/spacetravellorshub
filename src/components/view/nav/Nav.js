18f6ea9fb4a5315b43e8d783ba61af68b8ab5548
yayner <yayner2002@gmail.com>
2022-08-23 11:47:27 +0300

    Add active style to the nav links

--- src/components/view/nav/Nav.js
+++ src/components/view/nav/Nav.js
@@ -3,35 +3,54 @@ import { NavLink } from 'react-router-dom';
 import planet from './planet.png';
 import navStyle from './Nav.module.css';
 
-const Nav = () => (
-  <nav className={navStyle.nav}>
-    <div className={navStyle.navLeft}>
-      <img src={planet} alt="logo" className={navStyle.logo} />
-      <navLink to="/">
-        <h1>Space Travellers Hub</h1>
-      </navLink>
-    </div>
-    <div className={navStyle.navRight}>
-      <ul className={navStyle.navLinks}>
-        <li>
-          <NavLink to="/" className={navStyle.navLink}>
-            Rockets
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="missions" className={navStyle.navLink}>
-            Missions
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="myprofile" className={navStyle.navLink} id={navStyle.profile}>
-            Profile
-          </NavLink>
-        </li>
-      </ul>
-
-    </div>
-  </nav>
-);
+const Nav = () => {
+  const activeStyle = {
+    textDecoration: 'underline',
+  };
+  return (
+    <nav className={navStyle.nav}>
+      <div className={navStyle.navLeft}>
+        <NavLink to="/">
+          <img src={planet} alt="logo" className={navStyle.logo} />
+        </NavLink>
+        <NavLink to="/">
+          <h1>Space Travellers Hub</h1>
+        </NavLink>
+      </div>
+      <div className={navStyle.navRight}>
+        <ul className={navStyle.navLinks}>
+          <li>
+            <NavLink
+              to="/"
+              className={navStyle.navLink}
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="missions"
+              className={navStyle.navLink}
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="myprofile"
+              className={navStyle.navLink}
+              id={navStyle.profile}
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Profile
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
