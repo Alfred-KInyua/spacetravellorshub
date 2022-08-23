adfaade76cc32733868a03e03f999590a902ca10
yayner <yayner2002@gmail.com>
2022-08-23 16:03:06 +0300

    Fetch data from the missins endpoint

--- src/components/view/missions/Mission.js
+++ src/components/view/missions/Mission.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../../../redux/Missions';
 
-const Mission = () => (
-  <div>
-    <h1>Missions Page</h1>
-  </div>
-);
+const Mission = () => {
+  const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, [dispatch]);
+
+  return (
+    <>
+      {missions[0].map((mission) => <h1 key={mission.id}>{mission.mission_name}</h1>)}
+    </>
+  );
+};
 
 export default Mission;
--- src/redux/Missions.js
+++ src/redux/Missions.js
@@ -1,7 +1,23 @@
-import React from 'react';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const Missions = () => (
-  <div />
+const missionsApi = 'https://api.spacexdata.com/v3/missions';
+export const fetchMissions = createAsyncThunk(
+  'fetchMissions',
+  async () => {
+    const response = await axios.get(`${missionsApi}`);
+    return response.data;
+  },
 );
+const initialState = [];
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  extraReducers: {
+    // eslint-disable-next-line no-return-assign
+    [fetchMissions.fulfilled]: (state, { payload }) => [...state, payload],
+  },
+});
 
-export default Missions;
+export default missionsSlice.reducer;
