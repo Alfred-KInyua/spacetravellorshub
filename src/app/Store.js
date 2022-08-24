418e852f5e8de7c19021d6d93ed8bb28b93430b5
Kinyua <alfkinyua@gmail.com>
2022-08-24 20:43:16 +0300

    Add Redux store for Rockets and components

--- src/app/Store.js
+++ src/app/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from '../redux/Missions';
+import rocketsReducer from '../redux/rockets';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockets: rocketsReducer,
   },
 });
 
