e80db8a0e737ce4d317c0d683252cc8733f556fb
yayner <yayner2002@gmail.com>
2022-08-26 11:26:09 +0300

    Fix issues in the failing tests

--- src/components/view/_test_/Mission.test.js
+++ src/components/view/_test_/Mission.test.js
@@ -14,21 +14,3 @@ it('should match snapshot of the mission component', () => {
   const tree = renderer.create(<MockMission />).toJSON();
   expect(tree).toMatchSnapshot();
 });
-
-const sampleMockData = {
-  mission_id: '9D1B7E0',
-  mission_name: 'Thaicom',
-  description:
-    'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-};
-
-const MockMissionData = (
-  <Provider store={store}>
-    <Mission missionData={sampleMockData} />
-  </Provider>
-);
-
-it('takes the correct snapshot of data', () => {
-  const tree = renderer.create(<MockMissionData />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
